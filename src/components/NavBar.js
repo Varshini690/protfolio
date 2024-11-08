6a3328ed9031a48325a4675555d71dee6a294e8c
varshini <110101779+Varshini690@users.noreply.github.com>
2024-11-08 21:47:25 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -37,7 +37,9 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
+            <Link to="/">
+      <img src={logo} alt="Logo" />
+    </Link>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
